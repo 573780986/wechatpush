84d76a998b6efc1c82a2fe9d59a795fc4799e9fb
573780986 <66517684+573780986@users.noreply.github.com>
2022-08-24 20:31:54 +0800

    Update wechatpush.py

--- wechatpush.py
+++ wechatpush.py
@@ -31,7 +31,7 @@ def get_weather():
         high = i.group('high').replace(' ', '')
         low = i.group('low').replace(' ', '')
         break
-    return weather1, weather2, high, low
+        return weather1, weather2, high, low
 
 
 def get_local_number():
@@ -67,10 +67,8 @@ weather = weather1 + '转' + weather2
 temp = low + '~' + high
 local_number = get_local_number()
 words = get_words()
-data = {
-    "local_number": {"value": local_number, "color": get_random_color()},
-    "weather": {"value": weather, "color": get_random_color()},
-    "temp": {"value": temp, "color": get_random_color()},
-    "words": {"value": words, "color": get_random_color()}
-}
+data = {"local_number": {"value": local_number, "color": get_random_color()},
+        "weather": {"value": weather, "color": get_random_color()},
+        "temp": {"value": temp, "color": get_random_color()},
+       "words": {"value": words, "color": get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
