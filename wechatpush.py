1ecbeca590afd32bafc1083398f26fe4c507c919
573780986 <66517684+573780986@users.noreply.github.com>
2022-08-24 20:20:58 +0800

    Update wechatpush.py

--- wechatpush.py
+++ wechatpush.py
@@ -9,32 +9,29 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36 Edg/103.0.1264.77"
 }
 
-city = '重庆'
-
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
 
 user_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
 
-obj1 = re.compile(
-    r'"(?P<city>.*?)":"重庆","lastUpdateTime":".*?","date":".*?","weather":"(?P<weather>.*?)","temp":(?P<temp>.*?),"humidity":".*?","wind":".*?","pm25":.*?,"pm10":.*?,"low":(?P<low>.*?),"high":(?P<high>.*?),"airData":".*?","airQuality":"(?P<airQuality>.*?)","dateLong":.*?,"weatherType":.*?,"windLevel":.*?,"province":"重庆"')
+obj1 = re.compile(r'<br>.*?</div>.*?<div class="day-item dayicon">.*?<img src=".*?">.*?</div>.*?<div class="day-item">(?P<day_item1>.*?)</div>.*?<div class="day-item">.*?</div>.*?<div class="day-item">.*?</div>.*?<div class="day-item bardiv">.*?<div class="bar" style="top:0px; bottom:0px;">.*?<div class="high">.*?(?P<high>.*?)</div>.*?<div class="low">.*?(?P<low>.*?)</div>.*?</div>.*?</div>.*?<div class="day-item nighticon">.*?<img src=".*?">.*?</div>.*?<div class="day-item">(?P<day_item2>.*?)</div>.*?<div class="day-item">.*?</div>.*?<div class="day-item">.*?</div>.*?</div>.*?<div class="pull-left day ">.*?<div class="day-item">.*?',re.S)
 obj2 = re.compile(r'"name":"新增本土","data":\[.*,(?P<local_number>.*?)\]}\],"updateDate"')
 obj3 = re.compile(r'"data":"(?P<data>.*?)"')
 
 
 def get_weather():
-    url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
+    url = "https://weather.cma.cn/web/weather/57516"
     resp = requests.get(url, headers=headers)
     resp.close()
     result1 = obj1.finditer(resp.text)
     for i in result1:
-        weather = i.group('weather')
-        temp = i.group('temp')
-        high = i.group('high')
-        low = i.group('low')
-        airQuality = i.group('airQuality')
-    return weather, temp, high, low, airQuality
+        weather1 = i.group('day_item1').replace(' ', '')
+        weather2 = i.group('day_item2').replace(' ', '')
+        high = i.group('high').replace(' ', '')
+        low = i.group('low').replace(' ', '')
+        break
+    return weather1, weather2, high, low
 
 
 def get_local_number():
@@ -65,14 +62,13 @@ def get_random_color():
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
-weather, temp, high, low, airQuality = get_weather()
+weather1, weather2, high, low = get_weather()
+weather = weather1 + '转' + weather2
+temp = low + '~' + high
 local_number = get_local_number()
 words = get_words()
 data = {"local_number": {"value": local_number, "color": get_random_color()},
         "weather": {"value": weather, "color": get_random_color()},
         "temp": {"value": temp, "color": get_random_color()},
-        "airQuality": {"value": airQuality, "color": get_random_color()},
         "words": {"value": words, "color": get_random_color()},
-        "high": {"value": high, "color": get_random_color()},
-        "low": {"value": low, "color": get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
